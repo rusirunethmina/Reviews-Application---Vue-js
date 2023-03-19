e260cba425ccd7c083644057cb9271ae99effa49
Rusiru1998 <rusiruc21@gmail.com>
2023-03-19 22:27:24 +0530

    setup home page

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div class="containner">
+      <div class="nav-inner">
+        <h2>Reviews UI</h2>
+        <nav>
+          <router-link to="/"> Home </router-link>
+          <router-link to="/about"> About </router-link>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script></script>
+
+<style></style>
